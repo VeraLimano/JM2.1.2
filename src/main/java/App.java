7a242c2163f6a10c2c45a1e1f09d96fdb656e37e
VeraLimano <69342940+VeraLimano@users.noreply.github.com>
2020-11-07 19:31:57 +0300

    first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1 == bean2);
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Cat.class);
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean1 == catBean2);
+
     }
 }
